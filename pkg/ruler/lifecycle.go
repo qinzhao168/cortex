54c7cdf4379ca018238e9fbad66676fef04d8b54
Jacob Lisi <jacob.t.lisi@gmail.com>
2019-04-10 14:25:14 -0400

    convert TransferOut to noop for the ruler since no transfer is required
    Signed-off-by: Jacob Lisi <jacob.t.lisi@gmail.com>

--- pkg/ruler/lifecycle.go
+++ pkg/ruler/lifecycle.go
@@ -2,10 +2,8 @@ package ruler
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	"github.com/cortexproject/cortex/pkg/ring"
 	"github.com/cortexproject/cortex/pkg/util"
 	"github.com/go-kit/kit/log/level"
 )
@@ -14,49 +12,9 @@ const (
 	pendingSearchIterations = 10
 )
 
-// TransferOut finds an ingester in Active state and terminates upon finding it
+// TransferOut is a noop for the ruler
 func (r *Ruler) TransferOut(ctx context.Context) error {
-	_, err := r.findTargetRuler(ctx)
-	if err != nil {
-		return fmt.Errorf("cannot find replacement ruler: %v", err)
-	}
-
 	return nil
-}
-
-// findTargetRuler finds an ingester in PENDING state. Before terminating
-// the ruler must be sure there is a replacement ruler being created
-func (r *Ruler) findTargetRuler(ctx context.Context) (*ring.IngesterDesc, error) {
-	level.Debug(util.Logger).Log("msg", "looking for pending rulers")
-	findRuler := func() (*ring.IngesterDesc, error) {
-		ringDesc, err := r.lifecycler.KVStore.Get(ctx, ring.ConsulKey)
-		if err != nil {
-			return nil, err
-		}
-
-		rulers := ringDesc.(*ring.Desc).FindIngestersByState(ring.PENDING)
-		if len(rulers) <= 0 {
-			return nil, fmt.Errorf("no pending rulers")
-		}
-
-		return &rulers[0], nil
-	}
-
-	deadline := time.Now().Add(r.cfg.SearchPendingFor)
-	for {
-		ingester, err := findRuler()
-		if err != nil {
-			level.Debug(util.Logger).Log("msg", "Error looking for active rulers", "err", err)
-			if time.Now().Before(deadline) {
-				time.Sleep(r.cfg.SearchPendingFor / pendingSearchIterations)
-				continue
-			} else {
-				level.Warn(util.Logger).Log("msg", "Could not find pending ruler before deadline", "err", err)
-				return nil, err
-			}
-		}
-		return ingester, nil
-	}
 }
 
 // StopIncomingRequests is called during the shutdown process.

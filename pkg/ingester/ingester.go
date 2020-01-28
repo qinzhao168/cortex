9794e256dadecd3eec28436c73b776cced2b66e5
Owen Diehl <ow.diehl@gmail.com>
2020-01-28 10:08:50 -0500

    removes unnecessary codec interface in astmapping
    Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -437,7 +437,6 @@ func (i *Ingester) Query(ctx old_ctx.Context, req *client.QueryRequest) (*client
 	result := &client.QueryResponse{}
 	numSeries, numSamples := 0, 0
 	maxSamplesPerQuery := i.limits.MaxSamplesPerQuery(userID)
-
 	err = state.forSeriesMatching(ctx, matchers, func(ctx context.Context, _ model.Fingerprint, series *memorySeries) error {
 		values, err := series.samplesForRange(from, through)
 		if err != nil {
